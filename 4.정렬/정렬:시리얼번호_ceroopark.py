9c4dba0a482f3bde3f41e2a662a9ce4e3da70a86
aboutkunyoung <aboutkunyoung@gmail.com>
2024-04-03 21:37:48 +0900

    2024-04-03

--- 4.정렬/정렬:시리얼번호_ceroopark.py
+++ 4.정렬/정렬:시리얼번호_ceroopark.py
@@ -5,38 +5,13 @@ def sum(string):
             result+=int(s)
     return result
 
-def compare(str1, str2):
-    #1
-    if len(str1)<len(str2):
-        return True
-    elif len(str1)>len(str2):#not else cause if using else this def can't be over
-        return False
-    #2
-    if sum(str1)<sum(str2):
-        return True
-
-    elif sum(str1)>sum(str2):
-        return False
-    #3
-    for i in range(len(str1)):
-        if ord(str1[i]) < ord(str2[i]):
-            return True
-        elif ord(str1[i]) > ord(str2[i]):
-            return False
-
-def bubble_sort(a):
-    for i in range(0, len(a)):
-        for j in range(len(a)-1, i, -1):
-            if compare(a[j-1], a[j])==False:
-                a[j-1], a[j] = a[j], a[j-1]
-                
 N=int(input())
 products=[]
 
 for _ in range(N):
     products.append(input())
 
-bubble_sort(products)
+products.sort(key = lambda x:(len(x), sum(x), x))
 
 for s in products:
     print(s)
