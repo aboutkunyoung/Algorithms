ea458969fffe2735f4a3bb25cb86bf2f6b6147d4
aboutkunyoung <aboutkunyoung@gmail.com>
2024-08-13 22:56:13 +0900

    0813

---
+++ BOJ/8.재귀/BOJ_15654.java
@@ -0,0 +1,62 @@
+package BOJ;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_15654 {
+
+	static BufferedReader br = null;
+	static BufferedWriter bw = null;
+	static StringTokenizer st = null;
+	static StringBuilder sb = null;
+	static int N, M;
+	static int arr[];
+	static boolean[] v;
+
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		int[] sel = new int[M];
+		v = new boolean[N];
+		recursive(0, sel, v);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	private static void recursive(int k, int sel[], boolean[] v) {
+		if (k == sel.length) {
+			for (int i = 0; i < sel.length; i++) {
+				sb.append(sel[i]).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+		for (int i = 0; i < N; i++) {
+			if (v[i] == false) {
+				v[i] = true;
+				sel[k] = arr[i];
+				recursive(k + 1, sel, v);
+				v[i] = false;
+			}
+		}
+
+	}
+
+}
